3fbda18b462ee9364ea01b5078bbc546a735ce2b
Kyle Vedder <kyle.c.vedder@gmail.com>
2017-12-26 13:29:15 -0500

    Added recursive clone to ensure submodules are cloned properly.

--- easy_install.sh
+++ easy_install.sh
@@ -1,1 +1,1 @@
-cd ~ && sudo apt -y install git && git clone https://github.com/kylevedder/TheLazarusProject.git && mv TheLazarusProject lazarus
+cd ~ && sudo apt -y install git && git clone --recursive https://github.com/kylevedder/TheLazarusProject.git && mv TheLazarusProject lazarus
