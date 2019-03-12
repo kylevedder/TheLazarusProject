d15e9d6071cd02fc6b5d4b54e03a271b3f4409e4
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-03-12 15:01:10 -0400

    Fixed install so it goes even smoother.

--- easy_install.sh
+++ easy_install.sh
@@ -1,1 +1,1 @@
-cd ~ && sudo apt -y install git && git clone --recursive https://github.com/kylevedder/TheLazarusProject.git lazarus
+cd ~ && sudo apt update && sudo apt -y install git && git clone --recursive https://github.com/kylevedder/TheLazarusProject.git lazarus
