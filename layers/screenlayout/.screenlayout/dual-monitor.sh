c6ca40518ab102844963cfb959d988848bda2024
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-04-27 00:00:01 -0400

    Added updates to screenlayouts

--- layers/screenlayout/.screenlayout/dual-monitor.sh
+++ layers/screenlayout/.screenlayout/dual-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2 --mode 1920x1080 --pos 1080x0 --rotate right
+xrandr --output eDP-1 --off --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3 --off --output DP-2 --mode 1920x1080 --pos 1080x0 --rotate right --output DP-1 --off
