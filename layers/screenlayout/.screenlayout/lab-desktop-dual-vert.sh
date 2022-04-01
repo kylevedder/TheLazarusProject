4ebe8074866f30038e95d1b1bc642accbd99489f
Kyle Vedder <kyle.c.vedder@gmail.com>
2022-03-31 20:08:08 -0400

    Added dual vert

---
+++ layers/screenlayout/.screenlayout/lab-desktop-dual-vert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 1080x0 --rotate right --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 0x0 --rotate left --output DP-4 --off --output DP-5 --off --output DP-6 --off --output DP-7 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
