9ecb1ba0182069cffabf31a449c160357e6036ae
Kyle Vedder <kyle@dynarobotics.ai>
2025-01-21 18:51:54 -0800

    Added dual vert

---
+++ layers/screenlayout/.screenlayout/dyna-dual-vert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 1440x0 --rotate right --output DP-0 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
